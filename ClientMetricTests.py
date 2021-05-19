9634ef1e369d3aa33aaa468cb7c0b713c9a1cfd7
Vet <vd188@yandex.ru>
2021-05-19 15:40:29 +0300

    My client (good)

--- ClientMetricTests.py
+++ ClientMetricTests.py
@@ -11,9 +11,6 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.client = Client('127.0.0.1', 1001)
 
-    #def test_something(self):
-    #    self.assertEqual(True, False)
-
     def mocked_get_data(self):
         return 'ok\npalm.cpu 0.5 1150864248\npalm.cpu 2.0 1150864247\neardrum.cpu 3.0 1150864250\n\n'
 
@@ -24,9 +21,6 @@ class MyTestCase(unittest.TestCase):
     @patch('solution.Client.get_data', mocked_get_data)
     def test_client_get(self):
         self.assertEqual(self.client.get('*'), {'palm.cpu': [(1150864247, 2.0), (1150864248, 0.5)], 'eardrum.cpu': [(1150864250, 3.0)]})
-
-
-
 
 
 if __name__ == '__main__':
--- solution.py
+++ solution.py
@@ -25,38 +25,45 @@ class Client:
             raise ClientError()
 
         response_str = self.get_data()
-        list_lines = response_str.split('\n')
 
-        if list_lines == "ok\n\n":
+        if response_str == "ok\n\n":
             return dict()
 
-        if list_lines == 'error\nwrong command\n\n':
+        if response_str == 'error\nwrong command\n\n':
             raise ClientError()
 
+        list_lines = response_str.split('\n')
         metric_dict = dict()
 
-        for line in list_lines[1:]:
-            metric_obs = line.split(' ')
-            if metric_obs[0] == '':
-                break
+        if list_lines[0] != 'ok':
+            raise ClientError()
 
-            if metric_obs[0] in metric_dict:
-                metric_dict[metric_obs[0]].append((int(metric_obs[2]),float(metric_obs[1])))
-                metric_dict[metric_obs[0]] = list(sorted(metric_dict[metric_obs[0]], key=lambda item: item[0]))
-            else:
-                metric_dict[metric_obs[0]] = [(int(metric_obs[2]),float(metric_obs[1]))]
+        try:
+            for line in list_lines[1:]:
+                metric_obs = line.split(' ')
+                if metric_obs[0] == '':
+                    break
+
+                if metric_obs[0] in metric_dict:
+                    metric_dict[metric_obs[0]].append((int(metric_obs[2]),float(metric_obs[1])))
+                    metric_dict[metric_obs[0]] = list(sorted(metric_dict[metric_obs[0]], key=lambda item: item[0]))
+                else:
+                    metric_dict[metric_obs[0]] = [(int(metric_obs[2]), float(metric_obs[1]))]
+        except:
+            raise ClientError
+
         return metric_dict
 
     def put(self, metric, value, timestamp = None):
         """put palm.cpu 23.7 1150864247\n"""
-        if (timestamp == None):
+        if timestamp == None:
             timestamp = int(time.time())
 
         if (self.send_data("put {} {} {}\n".format(metric, value, timestamp))) != None:
             raise ClientError()
 
-        #response_str = self.get_data()
+        response_str = self.get_data()
 
-        #if (response_str != 'ok\\n\\n')
-        #    raise ClientError()
+        if response_str != 'ok\n\n':
+           raise ClientError()
 
